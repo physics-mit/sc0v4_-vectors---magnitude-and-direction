1f228df3156d4ac961648fec2b90627266464472
book-g <gobalkrishnan.books@gmail.com>
2025-06-09 19:27:20 +0530

    4

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/sc0v4:-vectors---magnitude-and-direction/',  // Replace <repo-name> with your actual GitHub repo name
+  base: '/sc0v4_-vectors---magnitude-and-direction/',  // Replace <repo-name> with your actual GitHub repo name
   plugins: [react()]
 })
